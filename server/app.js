ba661df8464826d838f9657be5d716b4c5c79df9
technofra-govind <govind@technofra.com>
2024-01-29 15:44:25 +0530

    initial commmit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express();
+const cors = require("cors")
+require("dotenv").config();
+
+// middleware
+const corOptions = {
+    origin:"http://localhost:3000/"
+}
+app.use(express.json())
+app.use(cors())
+
+
+// Connect mongoDB
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    const PORT = process.env.PORT || 8000
+    app.listen(PORT,()=>{
+        console.log(`App is Listening on PORT ${PORT}`)
+    })
+}).catch(err=>{
+    console.log(err);
+})
+
+app.get("/",(req,res)=>{
+    res.status(201).json({message:"Connected to backend !"})
+})
